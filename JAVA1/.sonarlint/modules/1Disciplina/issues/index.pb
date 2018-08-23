3a8741c57fe29dbcb571970fff0c764c3be77f29
Geraldo Ribeiro <inf_gribeiro@uolinc.com>
2018-08-23 15:29:17 -0300

    Unidade 2 | Sintaxe JAVA

---
+++ JAVA1/.sonarlint/modules/1Disciplina/issues/index.pb
@@ -0,0 +1,19 @@
+
+O
+src/unidade1/HelloWorldApp.java,4\8\48e4e32a7afd5e22e02b7d5c1706b49e757ea179
+L
+src/unidade2/ExemploApp.java,8\1\8101e10cd8b1ac83a956fcfff8e96a39a45dd3ab
+W
+'src/unidade2/FaturamentoTrimestral.java,8\7\878b1f0af809462838cb58d44c218349cd1709ee
+L
+src/unidade2/Operadores.java,4\e\4ed9cbcaa04dbc20702ee7a40a48e74220407614
+K
+src/unidade2/Avaliacao.java,5\e\5ef6553a73664f2928d88b3e706bb7721c29542c
+U
+%src/unidade2/EstruturasRepeticao.java,f\3\f3bec6c7efa075663a3dcb121b595f854116e9b3
+P
+ src/unidade2/AplicacaoMedia.java,f\c\fca01df81bdc9c4281af850caed4f70a5676fdec
+S
+#src/unidade2/AplicacaoExpoente.java,b\c\bccfe8081ad49fc09d93120514cbe3c35fccc0fe
+T
+$src/unidade2/AplicacaoFibonacci.java,3\2\32ef7906885211fd9103751f31f3f8a31b3f815d
---
+++ JAVA1/1Disciplina/src/unidade2/Operadores.java
@@ -0,0 +1,28 @@
+package unidade2;
+
+import java.util.Scanner;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Informe o 1° valor: ");
+		double numero1 = input.nextInt();
+		System.out.print("Informe o 2° valor: ");
+		double numero2 = input.nextInt();
+		
+		//Operadores relacionais
+		
+		System.out.printf("%.2f > %.2f ? %b\n%.2f = %.2f ? %b\n%.2f < %.2f ? %b", numero1, numero2, (numero1 > numero2), numero1, numero2, (numero1 == numero2), numero1, numero2, (numero1 < numero2));
+		
+		//Operadores aritméticos
+		
+		/*System.out.println(String.format("Soma = %.2f\nSubtração = %.2f\nMultiplicação = %.2f\nDivisão = %.2f", numero1 + numero2, numero1 - numero2, numero1 * numero2, numero1 / numero2));*/
+		
+		input.close();
+
+	}
+
+}
